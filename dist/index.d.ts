abde56b855f73c22e4c244e16030399b383a38ee
fagnermeds <fagnermeds@gmail.com>
2021-12-20 21:23:58 -0300

    fix: change types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import { errors } from './errors';
+import { InvalidCredential } from './errors/InvalidCrendetial';
 
 interface PersonData {
     name: string;
@@ -6,7 +6,10 @@ interface PersonData {
     email: string;
 }
 
+export declare const errors: {
+    InvalidCredential: typeof InvalidCredential;
+};
+
 export declare const slogger: {
     logPersonData: (personData: PersonData) => void;
 };
-export { errors };
